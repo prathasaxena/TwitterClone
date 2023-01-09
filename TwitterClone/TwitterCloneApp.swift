70217b64ac0e54f9acc332a4accd27b569dd2e08
prathasaxena <prathasaxena30@gmail.com>
2023-01-09 19:59:11 +0530

    Initial Commit

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by Pratha Saxena on 09/01/23.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
